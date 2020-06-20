436e524929935e7dab6846ed4e037ba2994c98a7
preetmodh <54811605+preetmodh@users.noreply.github.com>
2020-06-20 10:52:28 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -25,18 +25,23 @@ def Allcountries():
 
 @app.route("/scountries",methods=["GET","POST"])
 def scountries():
+    country=''
     country=request.form.get("name")
-    country=country.upper()
     res=requests.get('https://corona.lmao.ninja/v2/countries/'+country+'?yesterday')
     data=res.json()
-    cases="{:,}".format(data['cases'])
-    deaths="{:,}".format(data['deaths'])
-    todaycases="{:,}".format(data['todayCases'])
-    todaydeaths="{:,}".format(data['todayDeaths'])
-    todayrecovered="{:,}".format(data['todayRecovered'])
-    activecases="{:,}".format(data['active'])
-    recovered="{:,}".format(data['recovered'])
-    critical="{:,}".format(data['critical'])
-    mild="{:,}".format(data['active']-data['critical'])
-    outcome="{:,}".format(data['recovered']+data['deaths'])
-    return render_template("country.html",country=country,todayrecovered=todayrecovered,outcome=outcome,mild=mild,critical=critical,data=data,cases=cases,deaths=deaths,todaycases=todaycases,todaydeaths=todaydeaths,activecases=activecases,recovered=recovered)
+    country=data['country']
+    country=country.upper()
+    if res.status_code!=200:
+        return render_template("error.html")
+    else:
+        cases="{:,}".format(data['cases'])
+        deaths="{:,}".format(data['deaths'])
+        todaycases="{:,}".format(data['todayCases'])
+        todaydeaths="{:,}".format(data['todayDeaths'])
+        todayrecovered="{:,}".format(data['todayRecovered'])
+        activecases="{:,}".format(data['active'])
+        recovered="{:,}".format(data['recovered'])
+        critical="{:,}".format(data['critical'])
+        mild="{:,}".format(data['active']-data['critical'])
+        outcome="{:,}".format(data['recovered']+data['deaths'])
+        return render_template("country.html",country=country,todayrecovered=todayrecovered,outcome=outcome,mild=mild,critical=critical,data=data,cases=cases,deaths=deaths,todaycases=todaycases,todaydeaths=todaydeaths,activecases=activecases,recovered=recovered)
