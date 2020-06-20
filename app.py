766a79c5e78d260791c5eb79da8d4c3930db860f
preetmodh <54811605+preetmodh@users.noreply.github.com>
2020-06-20 10:21:31 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask,render_template,request
+import requests
+app = Flask(__name__, template_folder='template')
+@app.route("/",methods=["GET"])
+def index():
+    res=requests.get('https://corona.lmao.ninja/v2/all?yesterday')
+    data=res.json()
+    cases="{:,}".format(data['cases'])
+    deaths="{:,}".format(data['deaths'])
+    todaycases="{:,}".format(data['todayCases'])
+    todaydeaths="{:,}".format(data['todayDeaths'])
+    todayrecovered="{:,}".format(data['todayRecovered'])
+    activecases="{:,}".format(data['active'])
+    recovered="{:,}".format(data['recovered'])
+    critical="{:,}".format(data['critical'])
+    mild="{:,}".format(data['active']-data['critical'])
+    outcome="{:,}".format(data['recovered']+data['deaths'])
+    return render_template("index.html",todayrecovered=todayrecovered,outcome=outcome,mild=mild,critical=critical,data=data,cases=cases,deaths=deaths,todaycases=todaycases,todaydeaths=todaydeaths,activecases=activecases,recovered=recovered)
+
+@app.route("/Allcountries",methods=["GET","POST"])
+def Allcountries():
+    res=requests.get('https://corona.lmao.ninja/v2/countries?yesterday&sort')
+    datas=res.json()
+    return render_template("Allcountries.html",datas=datas)
+
+@app.route("/scountries",methods=["GET","POST"])
+def scountries():
+    country=request.form.get("name")
+    country=country.upper()
+    res=requests.get('https://corona.lmao.ninja/v2/countries/'+country+'?yesterday')
+    data=res.json()
+    cases="{:,}".format(data['cases'])
+    deaths="{:,}".format(data['deaths'])
+    todaycases="{:,}".format(data['todayCases'])
+    todaydeaths="{:,}".format(data['todayDeaths'])
+    todayrecovered="{:,}".format(data['todayRecovered'])
+    activecases="{:,}".format(data['active'])
+    recovered="{:,}".format(data['recovered'])
+    critical="{:,}".format(data['critical'])
+    mild="{:,}".format(data['active']-data['critical'])
+    outcome="{:,}".format(data['recovered']+data['deaths'])
+    return render_template("country.html",country=country,todayrecovered=todayrecovered,outcome=outcome,mild=mild,critical=critical,data=data,cases=cases,deaths=deaths,todaycases=todaycases,todaydeaths=todaydeaths,activecases=activecases,recovered=recovered)
